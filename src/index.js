1e4ca8fcb3ab852ed75ca7a1509e8e75c0a9cef4
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-15 14:46:12 -0500

    List Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("submit", function subClick(event) {
+  event.preventDefault();
+  const taskText = document.querySelector("#new-task-description")
+  const taskList = document.querySelector("#tasks")
+  const newTask = document.createElement("li");
+  newTask.textContent = taskText.value;
+  taskList.appendChild(newTask);
+}) 
 });
